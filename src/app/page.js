795062ac74053e99ffcca110fc7e9acf8a00b924
Harsh Pathak <jkpathak83195@gmail.com>
2024-10-30 10:15:40 +0530

    vid prototype added

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import styles from '@/styles/page-styles/home.module.css'
 import CategorySearchBox from '@/components/page-sections/utilities/CategorySearchBox'
 import ChooseCategory from '@/components/page-sections/utilities/ChoseCategory'
 import Sidebar from '@/components/page-sections/utilities/Sidebar'
+import ProductCard from '@/components/cards/ProductCard'
+import MainPage from '@/components/cards/mainPage'
 export default function page() {
   return (
     <>
@@ -16,6 +18,7 @@ export default function page() {
         <CategorySearchBox />
         {/* Category cards like Helmet, Tank, Bonnet to choose from */}
         <ChooseCategory />
+    <MainPage/>
 
       </main>
     </>
