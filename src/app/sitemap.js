78299377cdf91806ca61d5e9d09627be77dd95da
Lucky <luckysolanki902@gmail.com>
2024-11-20 20:05:38 +0530

    PastOrders now visible on viewCart as happycustomer is now available as a client comp

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -13,6 +13,12 @@ export default async function sitemap() {
         priority: 1,
       },
       {
+        url: `${baseUrl}/orders/track`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.7,
+      },
+      {
         url: `${baseUrl}/about-us`,
         lastModified: new Date(),
         changeFrequency: 'monthly',
---
+++ src/components/showcase/sliders/HappyCustomerClient.js
@@ -0,0 +1,60 @@
+"use client";
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import styles from './styles/happycustomers.module.css';
+
+export default function HappyCustomersClient({ parentSpecificCategoryId, noShadow, noHeading, headingText = 'Happy Customers' }) {
+  const baseImageUrl = process.env.NEXT_PUBLIC_CLOUDFRONT_BASEURL;
+  const [happyCustomers, setHappyCustomers] = useState([]);
+
+  const getFirstLetter = (name) => (name ? name[0] : '');
+
+  useEffect(() => {
+    async function fetchHappyCustomers() {
+      try {
+        const queryParam = parentSpecificCategoryId
+          ? `?parentSpecificCategoryId=${parentSpecificCategoryId}`
+          : '?homepage=true';
+        const response = await fetch(`/api/showcase/happy-customers${queryParam}`);
+        const data = await response.json();
+
+        if (data?.happyCustomers) {
+          setHappyCustomers(data.happyCustomers);
+        } else {
+          console.warn('No happy customers found');
+        }
+      } catch (error) {
+        console.error("Error fetching happy customers:", error);
+      }
+    }
+
+    fetchHappyCustomers();
+  }, [parentSpecificCategoryId]);
+
+  if (!happyCustomers.length) return null;
+
+  return (
+    <div className={`${styles.main} ${!noShadow && styles.shadow}`}>
+      <div className={styles.pastOrdersMain}>
+        {!noHeading && <div className={styles.pastOrdersH}>{headingText}</div>}
+      </div>
+      <div className={styles.slider}>
+        {happyCustomers.map((customer, index) => (
+          <div className={styles.slide} key={index}>
+            <Image
+              src={`${baseImageUrl}/${customer.photo}`.trim()}
+              alt={`${customer.name}'s photo`}
+              width={500}
+              height={500}
+              className={styles.image}
+            />
+            <div className={styles.details}>
+              <div className={styles.circle}>{getFirstLetter(customer.name)}</div>
+              <span className={styles.name}>{customer.name}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
