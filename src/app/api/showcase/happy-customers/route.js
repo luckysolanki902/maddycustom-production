3b2595751b20455a981be052cf0ac2bc91e32d20
Lucky <luckysolanki902@gmail.com>
2024-12-11 13:16:01 +0530

    fixed happycustomer not showing on productIdpage bug

--- src/app/api/showcase/happy-customers/route.js
+++ src/app/api/showcase/happy-customers/route.js
@@ -12,17 +12,11 @@ export async function GET(req) {
   try {
     console.info('Attempting to connect to the database...');
     await connectToDatabase();
-    console.info('Database connection established successfully.');
-
     let filter = { isActive: true }; // Base filter for active happy customers
 
     if (homepage === 'true') {
-      console.info('Fetching happy customers flagged for the homepage.');
       filter = { ...filter, showOnHomepage: true };
     } else if (parentSpecificCategoryId) {
-      console.info(
-        `Fetching happy customers for category ID: ${parentSpecificCategoryId}, including global ones.`
-      );
       filter = {
         ...filter,
         $or: [
@@ -38,12 +32,10 @@ export async function GET(req) {
       );
     }
 
-    console.info('Executing query to fetch happy customers with filter:', filter);
     const happyCustomers = await HappyCustomer.find(filter)
       .sort({ isGlobal: -1, globalDisplayOrder: 1, 'placements.displayOrder': 1 }) // Global and homepage customers first
       .select('name photo');
 
-    console.info(`Successfully fetched ${happyCustomers.length} happy customers.`);
     return NextResponse.json({ happyCustomers }, { status: 200 });
   } catch (error) {
     console.error('Error fetching happy customers:', error.message);
