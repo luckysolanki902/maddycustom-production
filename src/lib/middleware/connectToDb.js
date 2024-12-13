3fe228e9302829ba8554c4b47f39ead4e170eb62
Lucky <luckysolanki902@gmail.com>
2024-12-14 01:21:56 +0530

    merge with main

--- src/lib/middleware/connectToDb.js
+++ src/lib/middleware/connectToDb.js
@@ -7,6 +7,8 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
+// merging branch with main 
+
 let cached = global.mongoose;
 
 if (!cached) {
