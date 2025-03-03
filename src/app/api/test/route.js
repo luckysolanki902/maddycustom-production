642113935b3d328befb56d1c21a2a8e928e2be14
Lucky <luckysolanki902@gmail.com>
2025-03-03 23:57:55 +0530

    Adding twilio

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,7 +1,10 @@
-// pages/api/test/route.js
-
+// app/api/test-sms/route.js
 import { NextResponse } from 'next/server';
+import { sendSMS } from '@/lib/utils/twilioSender';
 
-export async function GET(req) {
-    return NextResponse.json({ message: 'success' }, { status: 200 });
+export async function GET() {
+  const testNumber = '+18777804236';
+  const testMessage = 'This is a test SMS from Twilio.hellow hello';
+  const result = await sendSMS({ to: testNumber, body: testMessage });
+  return NextResponse.json(result);
 }
