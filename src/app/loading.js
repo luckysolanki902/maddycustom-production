708c14620927df4258e68633213e5855fdab436d
Lucky <luckysolanki902@gmail.com>
2025-04-03 20:28:20 +0530

    fix: removed loading.js of helmet to reduce commulative layout shift and changed color of top loading bar to #424242 for the updated white topbar

--- src/app/loading.js
+++
@@ -1,9 +0,0 @@
-import PageLoading from '@/components/utils/PageLoading'
-import Image from 'next/image'
-import React from 'react'
-
-export default function loading() {
-    return (
-        <PageLoading />
-    )
-}
