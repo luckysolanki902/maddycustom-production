1b57eaba50c1d35b435b300fa384e5e6f63bcc98
Lucky <luckysolanki902@gmail.com>
2025-04-08 20:30:04 +0530

    feat: automated catalogue for meta with bath processing and cycled approach to generate and updated a feed collection

---
+++ src/models/meta/Catalogue.js
@@ -0,0 +1,38 @@
+// models/meta/Catalogue.js
+import mongoose from 'mongoose';
+
+const CatalogueSchema = new mongoose.Schema({
+  cycleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'CatalogueCycle',
+    required: true,
+  },
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  // If present, this entry was created for a product option.
+  optionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Option',
+  },
+  // Feed data to be used in the Facebook dynamic catalogue.
+  feedData: {
+    id: String, // a unique identifier (for example, productId or productId-optionId)
+    title: String,
+    description: String,
+    availability: String,
+    condition: String,
+    price: String,
+    link: String,
+    image_link: String,
+    brand: String,
+  },
+  processed: {
+    type: Boolean,
+    default: true,
+  },
+}, { timestamps: true });
+
+export default mongoose.models.Catalogue || mongoose.model('Catalogue', CatalogueSchema);
