47a5dd6f0ac64033b4c37a5e619a6876741967cc
Lucky <luckysolanki902@gmail.com>
2025-04-10 14:40:36 +0530

    sync: test and prod dbs and buckets are now synced

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ const [
 ] = await Promise.all([
   fetchOurUniqueProducts(),
   fetchRandomProducts('f', 10), // Pass your specific category slug here
-  fetchFeaturedproducts('f'),
+  fetchFeaturedproducts('caf'),
   fetchHappyCustomers(null),
   fetchSearchCategories(),
 ]);
