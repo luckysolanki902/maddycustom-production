c7215b1e7605d74c50480d61b54cc44f6ea3eab0
Lucky <luckysolanki902@gmail.com>
2025-05-05 03:47:07 +0530

    fix: merchant api now fetches all products at once and don't work in cycle

--- src/app/api/cron/meta/generate-catalogue/route.js
+++ src/app/api/cron/meta/generate-catalogue/route.js
@@ -7,7 +7,7 @@ import Option from '@/models/Option';
 import Catalogue from '@/models/meta/Catalogue';
 import CatalogueCycle from '@/models/meta/CatalogueCycle';
 import SpecificCategory from '@/models/SpecificCategory';
-import InventoryData from '@/models/Inventory';
+import Inventory from '@/models/Inventory';
 import SpecificCategoryVariant from '@/models/SpecificCategoryVariant';
 
 export async function GET(request) {
