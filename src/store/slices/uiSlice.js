4573208ee29e3a57829558892c840b934aca635d
Luckysolanki902 <luckysolanki902@gmail.com>
2025-09-02 13:27:34 +0530

    fix: images not rendering bug because of option schema not considered while fetching images

--- src/store/slices/uiSlice.js
+++ src/store/slices/uiSlice.js
@@ -58,7 +58,6 @@ const uiSlice = createSlice({
     
     // Recommendation drawer reducers
     openRecommendationDrawer(state, action) {
-      console.log('🔥 DEBUG: uiSlice - openRecommendationDrawer called with:', action.payload);
       state.isRecommendationDrawerOpen = true;
       state.recommendationProduct = action.payload?.product || null;
     },
