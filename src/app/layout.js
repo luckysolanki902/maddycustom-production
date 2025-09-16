b2abfe9dfd0f44f0212cba6fede0755fd29025e3
Luckysolanki902 <luckysolanki902@gmail.com>
2025-09-16 14:51:18 +0530

    feat: global navigation listener that closes all dialogs on redirection

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,7 @@ import CartInitializer from '@/components/utils/CartInitializer';
 import CouponTimerBanner from '@/components/showcase/banners/CouponTimerBanner';
 import ClientUIWrappers from '@/components/layouts/ClientUIWrappers';
 import FullPageLoader from '@/components/utils/FullPageLoader';
+import NavigationListener from '@/components/utils/NavigationListener';
 
 // Configure Krona One with its only available weight
 const kronaOne = Krona_One({
@@ -80,6 +81,7 @@ export default function RootLayout({ children }) {
       <body>
         <ReduxProvider>
           <ScrollProvider>
+            <NavigationListener />
             <UTMCapture />
             <CouponTimerBanner/>
             {/* <UTMLogger /> */}
