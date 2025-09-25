5bc737f095d60c84a428ec19e60ad495e8112ad4
Luckysolanki902 <luckysolanki902@gmail.com>
2025-09-25 13:44:55 +0530

    feat: GTM component head and body

--- src/components/layouts/AnalyticsHead.js
+++ src/components/layouts/AnalyticsHead.js
@@ -4,7 +4,8 @@ import React from 'react'
 import FacebookPixel from '../analytics/FacebookPixel'
 import FacebookClickIdHandler from '../analytics/FacebookClickIdHandler'
 import FacebookPageViewTracker from '../analytics/FacebookPageViewTracker'
-import GoogleAnalytics from '../analytics/GoogleAnalytics'
+import GoogleAnalyticsGA4 from '../analytics/GoogleAnalyticsGA4'
+import GoogleTagManager from '../analytics/GoogleTagManager'
 import Clarity from '../analytics/Clarity'
 import Razorpay from '../analytics/Razorpay'
 import { CssBaseline } from '@mui/material'
@@ -13,10 +14,11 @@ export default function AnalyticsHead() {
     return (
         <>
             <CssBaseline />
+            <GoogleTagManager />
             <FacebookPixel />
             <FacebookClickIdHandler />
             <FacebookPageViewTracker />
-            <GoogleAnalytics />
+            <GoogleAnalyticsGA4 />
             <Clarity />
             <Razorpay />
         </>
